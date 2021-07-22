2ba91da38177e377e37c2f99a97fe4ccfcc5d51d
heyongxian <heyongxian@baijia.com>
2021-07-22 16:31:18 +0800

    project init

---
+++ src/main/java/com/heyx/humr/HumrApplication.java
@@ -0,0 +1,13 @@
+package com.heyx.humr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumrApplication.class, args);
+    }
+
+}
