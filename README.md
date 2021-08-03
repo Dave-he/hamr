a2fedba31a5789a7cf9191c43116770832fcdc9a
heyongxian <heyongxian@baijia.com>
2021-08-03 10:58:49 +0800

    project init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Getting Started
+HFMC
+
+- infra
+- starter
+- tool
+- system
+- auth
+
+
+DDD:
+
+app:
+domain:
+infra:
+interfaces:
--- src/main/java/com/heyx/hfmc/HfmcApplication.java
+++ src/main/java/com/heyx/hfmc/HfmcApplication.java
@@ -1,13 +1,13 @@
-package com.heyx.humr;
+package com.heyx.hfmc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HumrApplication {
+public class HfmcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HumrApplication.class, args);
+        SpringApplication.run(HfmcApplication.class, args);
     }
 
 }
